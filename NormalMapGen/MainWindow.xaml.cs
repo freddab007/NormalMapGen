6585863ee799046042aabe5f9eff68e32a982a22
freddab007 <erwann.perrin.i2d9@gmail.com>
2024-12-17 12:32:13 +0100

    First Button OpenFile with the link

--- NormalMapGen/MainWindow.xaml.cs
+++ NormalMapGen/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,23 @@ namespace NormalMapGen
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        public void OpenImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPEG file (*.jpg/*jpeg)|*.jpg;*jpeg;*.jfif|PNG file (*.png)|*.png|BitMap file (*.bmp)|*.bmp";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string text = "";
+
+                using (StreamReader s = new StreamReader(openFileDialog.FileName))
+                {
+                    
+                }
+            }
         }
     }
 }
